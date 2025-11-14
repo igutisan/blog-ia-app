ef00dfe0463b84ac18c29e39dad55ea1788e6d23
Inder Gutierrez <igutierrezsandoval42@gmail.com>
2025-11-14 18:09:08 -0500

    update image response

--- app/dtos/blog_response_dto.py
+++ app/dtos/blog_response_dto.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class BlogResponseDTO(BaseModel):
     id: str
     category: str
     title: str
     content: str
+    image_url: Optional[str] = None
     seo_description: str
     author: str
